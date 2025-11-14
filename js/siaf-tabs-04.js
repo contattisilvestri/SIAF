17885a4ae251825a9c3f2ca4eb26cb6e20acac7a
Marco Silvestri <akira@Marco-Silvestri.local>
2025-11-14 17:51:15 +0100

    revert: rollback CSS consolidation - restore inline styles
    CRITICAL ROLLBACK:
    - Elementi grafici persi dopo consolidamento CSS
    - Ripristino CSS inline nell'HTML (stato funzionante)
    - Ripristino CSS esterni precedenti
    - Ripristino versione v2.19.2
    
    Investigherò il problema del consolidamento CSS e riproverò
    con approccio più conservativo.

--- js/siaf-tabs-04.js
+++ js/siaf-tabs-04.js
@@ -1,15 +1,15 @@
 // BLOCCO 1: Definizione classe principale e inizializzazione variabili
-// 🚀 VERSION: SIAF-v2.20.0-FINAL-2025-11-14-05:00
+// 🚀 VERSION: SIAF-v2.19.2-FINAL-2025-11-14-04:00
 
 // Sistema versioning dinamico
 window.SIAF_VERSION = {
     major: 2,
-    minor: 20,
-    patch: 0,
+    minor: 19,
+    patch: 2,
     date: '14/11/2025',
-    time: '05:00',
-    description: 'CSS Consolidation: Rimosso CSS inline, tutto in file esterno',
-    color: '#10B981'  // Green - Major refactoring
+    time: '04:00',
+    description: 'UI: Espansi container interni a 1600px (pratica, form, condizioni)',
+    color: '#FF9500'  // Orange - UI improvement
 };
 
 class SiafApp {
